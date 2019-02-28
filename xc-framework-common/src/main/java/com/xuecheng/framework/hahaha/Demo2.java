ca9c1ad3dce702cf23f636a713b392a96cd456cd
SC-201808111658\Administrator <15381002414@163.com>
2019-02-28 10:54:19 +0800

    测试回滚

---
+++ xc-framework-common/src/main/java/com/xuecheng/framework/hahaha/Demo2.java
@@ -0,0 +1,38 @@
+package com.xuecheng.framework.hahaha;
+
+public class Demo2 {
+    /**
+     *
+     *
+     * aaaaaaaaaaaa
+     * qqqqqqqqqqq
+     * fffffffffffa
+     * f
+     * asf
+     *
+     * saf
+     *
+     * as
+     * f
+     * a
+     * sf
+     * 
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     *
+     */
+}
