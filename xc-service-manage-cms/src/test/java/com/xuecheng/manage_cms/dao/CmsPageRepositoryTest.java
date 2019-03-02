73ab4f555325deb4016d128f1f8009953d5ba2e7
SC-201808111658\Administrator <15381002414@163.com>
2019-03-02 11:13:24 +0800

    添加页面增删改查接口功能

--- xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
+++ xc-service-manage-cms/src/test/java/com/xuecheng/manage_cms/dao/CmsPageRepositoryTest.java
@@ -5,9 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -41,6 +39,36 @@ public class CmsPageRepositoryTest {
         Pageable pageable = PageRequest.of(page,size);
         Page<CmsPage> all = cmsPageRepository.findAll(pageable);
         System.out.println(all);
+    }
+
+    //自定义条件查询测试
+    @Test
+    public void testFindAllByExample() {
+        //分页参数
+        int page = 0;//从0开始
+        int size = 10;
+        Pageable pageable = PageRequest.of(page,size);
+
+        //条件值对象
+        CmsPage cmsPage= new CmsPage();
+        //要查询5a751fab6abb5044e0d19ea1站点的页面
+//        cmsPage.setSiteId("5b30b052f58b4411fc6cb1cf");
+        //设置模板id条件
+//        cmsPage.setTemplateId("5ad9a24d68db5239b8fef199");
+        //设置页面别名
+        cmsPage.setPageAliase("轮播");
+        //条件匹配器
+//        ExampleMatcher exampleMatcher = ExampleMatcher.matching();
+//        exampleMatcher = exampleMatcher.withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching()
+                .withMatcher("pageAliase", ExampleMatcher.GenericPropertyMatchers.contains());
+        //ExampleMatcher.GenericPropertyMatchers.contains() 包含关键字
+//        ExampleMatcher.GenericPropertyMatchers.startsWith()//前缀匹配
+        //定义Example
+        Example<CmsPage> example = Example.of(cmsPage,exampleMatcher);
+        Page<CmsPage> all = cmsPageRepository.findAll(example, pageable);
+        List<CmsPage> content = all.getContent();
+        System.out.println(content);
     }
 
     //修改
